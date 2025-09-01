ff8ee7f1a511335d2618f60a2afaa656652041d2
ham144 <24434muhammad.yafizham@gmail.com>
2025-09-01 07:57:05 +0000

    Sep 1, 2025, 7:57 AM

--- frontend/src/components/StatusPreviewModal.jsx
+++ frontend/src/components/StatusPreviewModal.jsx
@@ -9,8 +9,6 @@ const FlowStatusModal = ({
 }) => {
   if (!selectedFlow) return null;
 
-  console.log(selectedFlow);
-
   return (
     <dialog id="statusmodal" className="modal">
       <div className="modal-box">
