5b69935a41c807ffb30550fef0f8d8722c96c9ca
ham144 <24434muhammad.yafizham@gmail.com>
2025-09-02 09:20:19 +0000

    Sep 2, 2025, 9:20 AM

--- backend/models/Customer.model.js
+++ backend/models/Customer.model.js
@@ -1,7 +1,18 @@
 import mongoose from "mongoose";
 
-//with coordinates
-const customerSchema = new mongoose.Schema({});
+const customerSchema = new mongoose.Schema(
+  {
+    No: String,
+    Name: String,
+    coordinates: {
+      type: "Point",
+      coordinates: [Number, Number], //longtitude, lattitude
+    },
+  },
+  { timestamps: true }
+);
+
+customerSchema.index({ No: 1 }, { unique: true });
 
 const Customer = mongoose.model("Customer", customerSchema);
 export default Customer;
