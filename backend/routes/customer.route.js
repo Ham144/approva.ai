cf0646b16c59aa6ab6c99c48bc59f32a5f92645b
ham144 <24434muhammad.yafizham@gmail.com>
2025-09-03 04:47:01 +0000

    Sep 3, 2025, 4:47 AM

--- backend/routes/customer.route.js
+++ backend/routes/customer.route.js
@@ -8,6 +8,41 @@ router.get("/list", async (req, res) => {
   return customer;
 });
 
-router.get;
+router.post("/checkRadius", async (req, res) => {
+  const { longtitude, lattitude } = req.body;
+  const customer = Customer.find({
+    location: {
+      $near: {
+        $geometry: {
+          type: "Point",
+          coordinates: [longtitude, lattitude],
+        },
+        $maxDistance: 1000,
+      },
+    },
+  });
+
+  return customer;
+});
+
+router.post("/create", async (req, res) => {
+  const { No, Name, coordinates } = req.body;
+
+  const isExist = await Customer.findOne({ No });
+  if (isExist) {
+    return res.status(400).json({ message: "No already exist" });
+  }
+  const customer = await Customer.create({ No, Name, coordinates });
+  return customer;
+});
+
+router.put("/update", async (req, res) => {
+  const { No, Name, coordinates } = req.body;
+  const customer = await Customer.findOneAndUpdate(
+    { No },
+    { Name, coordinates }
+  );
+  return customer;
+});
 
 export default router;
