8b64d2e906b459282cab84254560fd732a6dfcb4
ham144 <24434muhammad.yafizham@gmail.com>
2025-09-04 08:36:13 +0000

    Sep 4, 2025, 8:36 AM

--- frontend/src/components/TableInput.jsx
+++ frontend/src/components/TableInput.jsx
@@ -161,28 +161,20 @@ const TableInput = ({
                                     }
                                   />
                                 </label>
-                                <div className="form-control w-full max-w-xs">
-                                  <label className="label">
-                                    <span className="label-text">
-                                      Pick a file
-                                    </span>
-                                    <span className="label-text-alt">
-                                      Alt label
-                                    </span>
-                                  </label>
-                                  <input
-                                    type="file"
-                                    className="file-input file-input-bordered w-full max-w-xs"
-                                  />
-                                  <label className="label">
-                                    <span className="label-text-alt">
-                                      Alt label
-                                    </span>
-                                    <span className="label-text-alt">
-                                      Alt label
-                                    </span>
-                                  </label>
-                                </div>
+
+                                <input
+                                  type="file"
+                                  className="file-input w-full max-w-xs"
+                                  accept="image/*"
+                                  {...baseProps}
+                                  onChange={(e) =>
+                                    handleFileChange(
+                                      rIdx,
+                                      cIdx,
+                                      e.target.files[0]
+                                    )
+                                  }
+                                />
                               </>
                             )}
 
