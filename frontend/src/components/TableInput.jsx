dc76bd98e67b6b4b180209476e8718e71b4a01a7
ham144 <24434muhammad.yafizham@gmail.com>
2025-09-04 08:35:26 +0000

    Sep 4, 2025, 8:35 AM

--- frontend/src/components/TableInput.jsx
+++ frontend/src/components/TableInput.jsx
@@ -161,6 +161,28 @@ const TableInput = ({
                                     }
                                   />
                                 </label>
+                                <div className="form-control w-full max-w-xs">
+                                  <label className="label">
+                                    <span className="label-text">
+                                      Pick a file
+                                    </span>
+                                    <span className="label-text-alt">
+                                      Alt label
+                                    </span>
+                                  </label>
+                                  <input
+                                    type="file"
+                                    className="file-input file-input-bordered w-full max-w-xs"
+                                  />
+                                  <label className="label">
+                                    <span className="label-text-alt">
+                                      Alt label
+                                    </span>
+                                    <span className="label-text-alt">
+                                      Alt label
+                                    </span>
+                                  </label>
+                                </div>
                               </>
                             )}
 
